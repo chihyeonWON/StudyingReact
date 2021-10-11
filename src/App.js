3399b4687859e82c0f77d17f3d217797ad0da459
원치현 <heungeob1003@gmail.com>
2021-10-11 18:26:27 +0900

    Seperate Content Component to ./components/Content.js And import Content.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
-import TOC from './components/TOC.js';
 import Subject from './components/Subject.js';
+import TOC from './components/TOC.js';
+import Content from './components/Content.js';
 import './App.css';
-class Content extends Component{
-  render(){
-    return(
-      <article>
-            <h2>{this.props.title}</h2>
-            {this.props.desc}
-        </article>
-    );
-  }
-}
 class App extends Component{ 
   render() {
     return (
