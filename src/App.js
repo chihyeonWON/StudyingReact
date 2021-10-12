a2a796fc7fe7b61de8cd88f8d54a31ee18876e07
원치현 <heungeob1003@gmail.com>
2021-10-12 20:30:07 +0900

    Set props by state in Subject Component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import TOC from './components/TOC.js';
 import Content from './components/Content.js';
 import './App.css';
 class App extends Component{ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}  
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject> 
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject> 
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
