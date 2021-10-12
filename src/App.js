c22283e5c837241a713168d518e61444046444f0
원치현 <heungeob1003@gmail.com>
2021-10-12 21:03:32 +0900

    Add conditonal syntax to change Component's rendering result by state's mode

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ class App extends Component{
    } 
  }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode == 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode == 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -25,7 +33,7 @@ class App extends Component{
           sub={this.state.subject.sub}>
         </Subject> 
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
