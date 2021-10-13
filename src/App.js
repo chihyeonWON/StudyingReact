24e469c30e6ba0158c2f1dc5d4f16959670a3928
원치현 <heungeob1003@gmail.com>
2021-10-13 14:53:33 +0900

    Edit code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component{
   constructor(props) {
    super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -19,27 +19,27 @@ class App extends Component{
  }
   render() {
     var _title, _desc = null;
-    if(this.state.mode == 'welcome') {
+    if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if (this.state.mode == 'read') {
+    } else if (this.state.mode === 'read') {
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
     }
     return (
       <div className="App">
-        {/*<Subject 
+        <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
+        </Subject>
+        {/*<header>
           <h1><a href="/" onClick={function(e){
             console.log(e);
             e.preventDefault();
             this.setState({ mode:'welcome' });
           }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
-        </header>
+        </header> */}
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
