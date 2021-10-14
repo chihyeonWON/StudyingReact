d89bd074604a54ea219cd9214295748aff00d4f6
원치현 <heungeob1003@gmail.com>
2021-10-14 15:35:27 +0900

    Build onChangeMode function when onclick create, update, delete button

--- src/App.js
+++ src/App.js
@@ -57,7 +57,12 @@ class App extends Component{
             }.bind(this)
           } 
           data={this.state.contents}></TOC>
-        <Control></Control>
+        <Control onChangeMode = {
+          function(_mode) {
+            this.setState({
+              mode:_mode
+          });
+        }.bind(this`)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
