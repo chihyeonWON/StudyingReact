a4d57a57ba0f5531d9624019eed59541c4765186
원치현 <heungeob1003@gmail.com>
2021-10-15 16:24:05 +0900

    Add new data to contents array by using concat function that do not change original data

--- src/App.js
+++ src/App.js
@@ -42,12 +42,18 @@ class App extends Component{
         _article = <CreateContent onSubmit={function(_title, _desc) {
           // add content to this.state.contents
           this.max_content_id = this.max_content +1;
-          this.state.contents.push(
+          /*this.state.contents.push(
             {id:this.max_content_id, title: _title, desc:_desc}
           );
           this.setState({
             contents:this.state.contents
-          })
+          })*/ 
+          var _contents = this.state.contents.concat(
+             {id:this.max_content_id, title: _title, desc:_desc}
+          )
+          this.setState({
+            contents:_contents
+          });
           console.log(_title, _desc)
         }.bind(this)}></CreateContent>
     }
