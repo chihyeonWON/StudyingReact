5bcc49e4a83b8a5f26f506041722d6a22cd5d549
원치현 <heungeob1003@gmail.com>
2021-10-16 18:16:15 +0900

    Add state title that means this.props.data.title in UpdateContent Component and Use setState function to change data directly

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 class UpdateContent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            title : this.props.data.title
+        };
+    }
   render(){
-    console.log(this.props.data);
     return(
       <article>
             <h2>Update</h2>
@@ -16,12 +21,18 @@ class UpdateContent extends Component{
                 }.bind(this)}
             >
                 <p>
-                    <input type="text" name="title" placeholder="title">
-                    </input>
+                    <input 
+                        type="text"
+                        name="title"
+                        placeholder="title"
+                        value = {this.state.title}
+                        onChange = {function(e) {
+                            this.setState({ title:e.target.value});
+                        }.bind(this)}
+                    ></input>
                 </p>
                 <p>
                     <textarea name="desc" placeholder="description">
-
                     </textarea>
                 </p>
                 <p>
